675b06f612ae73f7f83c911636d0e68a836e14da
DeedWark <deedwark@hotmail.fr>
2020-10-03 18:02:01 +0200

    Increase conditions

--- ez-telnet.sh
+++ ez-telnet.sh
@@ -3,36 +3,24 @@
 #This script will perform TELNET in a sec
 
 RED='\033[1;31m'
-NO='\033[0m'
+END='\033[0m'
 
 read -r -p "SMTP server: " smtp
-if [ -z "$smtp" ]; then
-       echo -e "${RED}SMTP required!${NO}"
-       exit 1
-fi
-
+[[ -z "$smtp" ]] && echo -e "${RED}SMTP required!${END}" && exit 1
 read -r -p "HELO/EHLO: " helo
-
 read -r -p "MAIL FROM: " mfrom
-
 read -r -p "RCPT TO: " rto
-if [ -z "$rto" ]; then
-	echo -e "${RED}RCPT TO required!${NO}"
-	exit 1
-fi
-
+[[ -z "$rto" ]] && echo -e "${RED}RCPT TO required!${END}" && exit 1
 read -r -p "From: " from
 read -r -p "To: " to
 read -r -p "Date (leave empty for current date): " date
 read -r -p "Subject: " subject
 read -r -p "Message-ID (leave empty for random or type 0 for none): " mid
 read -r -p "Mailer (leave empty to put this one or type 0 for none): " xm
-
 [[ -z "$from" ]] && from="$mfrom"
 [[ -z "$to" ]] && to="$rto"
-[[ -z "$date" ]] && curdate="$(date)"
+[[ -z "$date" ]] && curdate="$(date -R)"
 [[ -z "$subject" ]] && subject=""
-
 { 
 	echo "EHLO ${helo}";
 	echo "MAIL FROM:<${mfrom}>";
@@ -43,7 +31,7 @@ read -r -p "Mailer (leave empty to put this one or type 0 for none): " xm
 	echo "From: ${from}";
 	echo "Subject: ${subject}";
 	if [ -z "$mid" ]; then
-		echo "Message-Id: <$(date -R|tr -d ' ,+():[a-z][A-Z]').$(shuf -i 1000-9999 -n 1)@$(hostname).thisdomain>" 
+		echo "Message-Id: <$(date "+%Y%m%d%H%M%S").$(shuf -i 1000-9999 -n 1)@$(hostname).thisdomain>" 
 	elif [ "$mid" == "0" ]; then
 		: 
 	fi;
